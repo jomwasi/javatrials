56a3da89b04dc22c9ba99a31637b2fbae1c24faf
jomwasi <jomwasi@gmail.com>
2022-10-22 19:57:25 +0300

    add files

---
+++ index.js
@@ -0,0 +1,22 @@
+const submit= () => {
+
+ //retrieve elements
+    
+const dateElement = document.getElementById("date")
+    
+const genderElement = document.getElementById("gender")
+     
+    //retrieving input values
+    
+const date = dateElement.value;
+    
+const gender = genderElement.value;
+    
+    //const male = maleElement.value
+    
+    
+const submit = [date,gender]
+     
+    
+console.log(submit)
+}
